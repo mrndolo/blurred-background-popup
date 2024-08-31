39751495d4256e93630b3a13e3af072deda89cc8
mrndolo <callistomutunga@gmail.com>
2024-08-30 23:36:03 -0400

    Selected body elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const containerEl = document.querySelector(".container");
+
+const btnEl = document.querySelector(".btn");
+
+const popupContainerEl = document.querySelector(".popup-container");
+
+const closeIconEl = document.querySelector(".close-icon");
