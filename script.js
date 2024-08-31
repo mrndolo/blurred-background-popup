710730a882c1b30adee66dafa89cc5588f906103
mrndolo <callistomutunga@gmail.com>
2024-08-30 23:36:34 -0400

    Added event listeners

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const btnEl = document.querySelector(".btn");
 const popupContainerEl = document.querySelector(".popup-container");
 
 const closeIconEl = document.querySelector(".close-icon");
+
+btnEl.addEventListener("click", () => {
+    containerEl.classList.add("active");
+    popupContainerEl.classList.remove("active");
+  });
+  
+  closeIconEl.addEventListener("click", () => {
+    containerEl.classList.remove("active");
+    popupContainerEl.classList.add("active");
+  });
